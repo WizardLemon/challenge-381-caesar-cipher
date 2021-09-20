38968c50fe62043e666b907f0da0835908f1880e
WizardLemon <nemanjalekic.ns@gmail.com>
2021-09-20 20:46:11 +0200

    Implement caesar function that passes challenge test cases

--- solution/main.py
+++ solution/main.py
@@ -5,7 +5,7 @@ alphabet_list = list(string.ascii_lowercase)
 
 def warmup(letter: str, shift_by: int) -> str:
     """
-    Caesar shift alphabet function
+    Caesar shift alphabet function, works for one letter
 
     :param letter: letter to shift
     :param shift_by: shift modulo
@@ -17,3 +17,18 @@ def warmup(letter: str, shift_by: int) -> str:
     result = alphabet_list[index_new]
 
     return result
+
+
+def caesar(original_string: str, shift_by: int) -> str:
+    """
+    Casar shift alphabet function, shifts an entire string
+
+    :param original_string: string to encrypt
+    :param shift_by: shift modulo
+    :return: resulting string from original string, shifted by modulo operator
+    """
+
+    char_list = [c for c in original_string]
+    shifted_char_list = [warmup(c, shift_by) for c in char_list]
+
+    return "".join(shifted_char_list)
--- solution/tests.py
+++ solution/tests.py
@@ -3,6 +3,10 @@ from main import *
 
 
 class WarmupTestCase(unittest.TestCase):
+    """
+    This class contains tests for the warmup section of the challenge.
+    """
+
     def test_1(self):
         expected = 'a'
         actual = warmup('a', 0)
@@ -42,6 +46,48 @@ class WarmupTestCase(unittest.TestCase):
     def test_7(self):
         expected = 'm'
         actual = warmup('q', 22)
+
+        self.assertEqual(expected, actual)
+
+
+class ChallengeTestCase(unittest.TestCase):
+    """
+    This class contains the tests for the actual challenge
+    """
+
+    def test_1(self):
+        expected = "b"
+        actual = caesar("a", 1)
+
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        expected = "bcda"
+        actual = caesar("abcz", 1)
+
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        expected = "vex"
+        actual = caesar("irk", 13)
+
+        self.assertEqual(expected, actual)
+
+    def test_4(self):
+        expected = "layout"
+        actual = caesar("fusion", 6)
+
+        self.assertEqual(expected, actual)
+
+    def test_5(self):
+        expected = "jgorevxumxgsskx"
+        actual = caesar("dailyprogrammer", 6)
+
+        self.assertEqual(expected, actual)
+
+    def test_6(self):
+        expected = "dailyprogrammer"
+        actual = caesar("jgorevxumxgsskx", 20)
 
         self.assertEqual(expected, actual)
 
